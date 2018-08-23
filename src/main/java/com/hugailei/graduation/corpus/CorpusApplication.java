c9f6a8dd9afbe8994e854722ce26c41c14a969e5
hugailei <hugailei@100tal.com>
2018-08-23 18:31:23 +0800

    新建项目

---
+++ src/main/java/com/hugailei/graduation/corpus/CorpusApplication.java
@@ -0,0 +1,12 @@
+package com.hugailei.graduation.corpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorpusApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hugailei/graduation/corpus/CorpusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hugailei.graduation.corpus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorpusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
